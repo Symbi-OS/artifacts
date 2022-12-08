15a91e5f5981547efe24bb17a2e56e72209e54a8
FlareCoding <albert.slepak@gmail.com>
2022-12-07 19:30:51 -0500

    modified the script to be able to dynamically control parameters

--- redis/run_redis_benchmarks.py
+++ redis/run_redis_benchmarks.py
@@ -2,12 +2,25 @@
 import os
 import csv
 import time
+import argparse
+parser = argparse.ArgumentParser()
 
-MAX_INSTANCES = 7
-SERVER_NODE = '192.168.122.18'
+parser.add_argument("-n", "--name", help="Name of the run", required=True)
+parser.add_argument("-s", "--server", help="IPv4 address of the server hosting redis instances", default="192.168.122.18")
+parser.add_argument("-t", "--ipc_threads", help="Threads to be launched by the IPC server")
+parser.add_argument("-m", "--mode", help="Specify 'ipc' in order to use IPC approach", default="normal")
+parser.add_argument("-i", "--iterations", help="Number of iterations for each run", type=int, default=5)
+parser.add_argument("-max_instances", "--max_instances", help="Maximium number of redis instances to scale to", type=int, default=7)
+
+args = parser.parse_args()
+
+MAX_INSTANCES = args.max_instances
+SERVER_NODE = args.server
 tmp_results_path = 'tmp_redis_results.csv'
-REDIS_BENCH_REQUESTS = 250000
-EXPERIMENT_TYPE = 'different_nodes_vm'
+REDIS_BENCH_REQUESTS = 500000
+EXPERIMENT_NAME = args.name
+ITERATIONS_PER_RUN = args.iterations
+IPC_SERVER_THREADS = args.ipc_threads
 
 IPC_SHORTCUT_LIB = './Symbi-OS/artifacts/ipc_interposer/ipc_shortcut.so'
 IPC_SERVER_BIN = './Symbi-OS/artifacts/ipc_interposer/server'
@@ -15,6 +28,16 @@ REDIS_BIN = './Symbi-OS/artifacts/redis/fed36/redis-server'
 REDIS_SERVER_ARGS = "--protected-mode no --save '' --appendonly no --port {} &> /dev/null"
 
 SHOULD_USE_IPC = False
+if args.mode == "ipc":
+    SHOULD_USE_IPC = True
+
+def print_experiment_header():
+    print(f'########### {EXPERIMENT_NAME} ###########')
+    print(f'\tServer Node         : {SERVER_NODE}')
+    print(f'\tMax Redis Instances : {MAX_INSTANCES}')
+    print(f'\tIterations Per Run  : {ITERATIONS_PER_RUN}')
+    print(f'\tUsing IPC           : {SHOULD_USE_IPC}')
+    print(f'\tIPC Server Threads  : {IPC_SERVER_THREADS}')
 
 def run_server_cmd(cmd: str, daemon: bool, local_daemon: bool = False):
     if daemon:
@@ -35,7 +58,11 @@ def run_n_redis_benchmarks(n: int):
 
     if SHOULD_USE_IPC:
         # Starting the IPC server
-        run_server_cmd(f'{IPC_SERVER_BIN} {n} &>/dev/null', True, True)
+        if IPC_SERVER_THREADS is not None:
+            run_server_cmd(f'{IPC_SERVER_BIN} {IPC_SERVER_THREADS} &>/dev/null', True, True)
+        else:
+            run_server_cmd(f'{IPC_SERVER_BIN} {n} &>/dev/null', True, True)
+
         time.sleep(3)
 
     for i in range(0, n):
@@ -63,7 +90,10 @@ def run_n_redis_benchmarks(n: int):
     run_server_cmd('bash -c \'pkill -9 redis-server\'', False)
     if SHOULD_USE_IPC:
         # Kill the IPC server
-        run_server_cmd(f'{IPC_SERVER_BIN}_killer {n} &>/dev/null', False)
+        if IPC_SERVER_THREADS is not None:
+            run_server_cmd(f'{IPC_SERVER_BIN}_killer {IPC_SERVER_THREADS} &>/dev/null', False)
+        else:
+            run_server_cmd(f'{IPC_SERVER_BIN}_killer {n} &>/dev/null', False)
     
     time.sleep(1)
 
@@ -90,21 +120,17 @@ def run_n_redis_benchmarks(n: int):
             f.write('\n')
 
     with open('redis_results.csv', 'a') as f:
-        f.write(f'{str(n)},{str(aggregate_throughput)},{EXPERIMENT_TYPE}\n')
+        f.write(f'{str(n)},{str(aggregate_throughput)},{EXPERIMENT_NAME}\n')
 
 ############################################################################
 
 if __name__ == '__main__':
+    print_experiment_header()
+
     run_idx = 1
     for n in range(1, MAX_INSTANCES + 1, 1):
         print(f'[*] ----- Running {n} redis-server instances -----')
-        #if run_idx < 3:
-        if True:
-            # For the first two runs, get a good number
-            # of iterations in to generate error bars.
-            for _ in range(0, 6):
-                run_n_redis_benchmarks(n)
-        else:
+        for _ in range(0, ITERATIONS_PER_RUN):
             run_n_redis_benchmarks(n)
 
         run_idx += 1
